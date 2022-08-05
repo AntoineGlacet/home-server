1611174faf2c00c18b009c76d38858d5a491e5d8
Antoine Glacet <glacet.antoine@gmail.com>
2022-08-05 15:53:31 +0900

    glances and node-red icon fix

--- start-all.sh
+++ start-all.sh
@@ -7,5 +7,5 @@ declare -a StringArray=( "tools" "media" "HA"  )
 # Loop over folders and reference the env-file
 # Iterate the string array using for loop
 for val in ${StringArray[@]}; do
-   docker compose --file $val/docker-compose.yml --env-file .env up -d
+   docker compose --file $val/docker-compose.yml --env-file .env up -d --remove-orphans
 done

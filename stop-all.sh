892f1335bfd43a8c8d6767140d58deda614ec48b
Antoine Glacet <glacet.antoine@gmail.com>
2022-12-10 16:06:44 +0900

    bash syntax

--- stop-all.sh
+++ stop-all.sh
@@ -2,10 +2,10 @@
 
 # list of all folders to consider
 # Declare an array of string with type
-declare -a StringArray=("HA" "media" "tools" )
- 
+declare -a StringArray=("HA" "media" "tools")
+
 # Loop over folders and reference the env-file
 # Iterate the string array using for loop
-for val in ${StringArray[@]}; do
-   docker compose --file $val/docker-compose.yml --env-file .env down
+for val in "${StringArray[@]}"; do
+   docker compose --file "$val"/docker-compose.yml --env-file .env down
 done
--- update-all.sh
+++ update-all.sh
@@ -2,13 +2,13 @@
 
 # list of all folders to consider
 # Declare an array of string with type
-declare -a StringArray=("HA" "media" "tools" )
- 
+declare -a StringArray=("HA" "media" "tools")
+
 # Loop over folders and reference the env-file
 # Iterate the string array using for loop
-for val in ${StringArray[@]}; do
-   docker compose --file $val/docker-compose.yml --env-file .env pull
-   docker compose --file $val/docker-compose.yml --env-file .env up -d
+for val in "${StringArray[@]}"; do
+   docker compose --file "$val"/docker-compose.yml --env-file .env pull
+   docker compose --file "$val"/docker-compose.yml --env-file .env up -d
 done
 
 # prune

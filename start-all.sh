4e183da9511791ed4bdfad81656e23ead04a71d7
Antoine Glacet <glacet.antoine@gmail.com>
2022-12-10 16:06:44 +0900

    bash syntax

--- start-all.sh
+++ start-all.sh
@@ -2,10 +2,10 @@
 
 # list of all folders to consider
 # Declare an array of string with type
-declare -a StringArray=( "tools" "media" "HA"  )
- 
+declare -a StringArray=("tools" "media" "HA")
+
 # Loop over folders and reference the env-file
 # Iterate the string array using for loop
-for val in ${StringArray[@]}; do
-   docker compose --file $val/docker-compose.yml --env-file .env up -d --remove-orphans
+for val in "${StringArray[@]}"; do
+   docker compose --file "$val"/docker-compose.yml --env-file .env up -d --remove-orphans
 done

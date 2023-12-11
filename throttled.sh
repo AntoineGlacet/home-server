ef8b948226d88c4525b74e37ded3e22729720768
Antoine Glacet <glacet.antoine@gmail.com>
2023-12-11 23:32:16 +0900

    cleanup

--- throttled.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-while true; do
-    throttled=$(vcgencmd get_throttled)
-    echo "$(date):$throttled" >>/home/ubuntu/throttled.log
-    if [ "$throttled" != "throttled=0x0" ]; then
-        echo "throttled!!!"
-    fi
-    sleep 0.5
-done
